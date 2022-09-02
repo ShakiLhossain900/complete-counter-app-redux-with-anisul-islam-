90b1cc63eb5bbe3a46cf716a803cf64010004daa
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-02 21:11:48 +0600

    adding get product and add product

--- mreducer.js
+++ mreducer.js
@@ -2,8 +2,20 @@
 const initialProductState = {
     products:['sugar','salt'],
     nubmerofProduct:2
+};
 
-}
+const getProducts = (product) =>{
+    return{
+        type: 'get_Product',
+    };
+};
+
+const addProduct = (product) =>{
+    return {
+        type: 'add_Product',
+        payload: product,
+    };
+};
 
 
 //cartReducer
