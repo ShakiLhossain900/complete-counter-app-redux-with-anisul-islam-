2a9eb307d48e8b0f5142a4302b44977c3a097c45
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-03 21:34:39 +0600

    git added

--- mreducer.js
+++ mreducer.js
@@ -1,43 +1,41 @@
-//product reducer 
+const { createStore } = require("redux");
+//product reducer
 const initialProductState = {
-    products:['sugar','salt'],
-    nubmerofProduct:2
+  products: ["sugar", "salt"],
+  nubmerofProduct: 2,
 };
 
-const getProducts = (product) =>{
-    return{
-        type: 'get_Product',
-    };
+const getProducts = (product) => {
+  return {
+    type: "get_Product",
+  };
 };
 
-const addProduct = (product) =>{
-    return {
-        type: 'add_Product',
-        payload: product,
-    };
+const addProduct = (product) => {
+  return {
+    type: "add_Product",
+    payload: product,
+  };
 };
 
-//product reducer 
-const productReducer = (state=initialProductState, action)=>{
-  switch(action.type){
-    case 'get_Product':
-        return{
-            ...state,
-        };
-    case 'add_Product':
-        return{
-           products : [...state.products, action.payload],
-           nubmerofProduct : state.nubmerofProduct + 1
-        };
+//product reducer
+const productReducer = (state = initialProductState, action) => {
+  switch (action.type) {
+    case "get_Product":
+      return {
+        ...state,
+      };
+    case "add_Product":
+      return {
+        products: [...state.products, action.payload],
+        nubmerofProduct: state.nubmerofProduct + 1,
+      };
     default:
-        state;
+      state;
   }
 };
 
-
 //cartReducer
 
-
-
-//store 
-const 
+//store
+const store = createStore(productReducer);
