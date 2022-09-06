08daf767e31f06b832dd499db5e3309854f12f32
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-06 22:50:29 +0600

    new added

--- index2.js
+++ index2.js
@@ -1,34 +1,31 @@
 const { createStore } = require("redux");
 
-
 // product states
 const initialProductState = {
   products: ["sugar", "salt"],
   numberOfProducts: 2,
 };
 
-
 // product actions
 const getProductAction = () => {
   return {
-    type: 'GET_PRODUCTS',
+    type: "GET_PRODUCTS",
   };
 };
 const addProductAction = (product) => {
   return {
-    type: 'ADD_PRODUCTS',
+    type: "ADD_PRODUCTS",
     payload: product,
   };
 };
 
-
 const productsReducer = (state = initialProductState, action) => {
   switch (action.type) {
-    case 'GET_PRODUCTS':
+    case "GET_PRODUCTS":
       return {
         ...state,
       };
-    case 'ADD_PRODUCTS':
+    case "ADD_PRODUCTS":
       return {
         products: [...state.products, action.payload],
         numberOfProducts: state.numberOfProducts + 1,
@@ -38,7 +35,6 @@ const productsReducer = (state = initialProductState, action) => {
       return state;
   }
 };
-
 
 const store = createStore(productsReducer);
 
