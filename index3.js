95355061c0969eebf93514f7ab2029d8b753488c
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-07 22:19:13 +0600

    re

--- index3.js
+++ index3.js
@@ -1,42 +1,38 @@
-//async actions - api calling 
+//async actions - api calling
 //api url -https://jsonplaceholder.typicode.com/todos
 //middleware - redux - thunk -
 //axious api -
 
-//states 
+//states
 const initialTodosState = {
-    todos: [],  ///data fetch korle jei array golo asbe ai todor modde agolo thakbe
-    isLoading: false,
-    error: null //kono error dora porle amara todos er modde rekhe debu
+  todos: [], ///data fetch korle jei array golo asbe ai todor modde agolo thakbe
+  isLoading: false,
+  error: null, //kono error dora porle amara todos er modde rekhe debu
 };
-
 
 ///actions
 
-const getTodosRequrest = () =>{
- return{
-    type: 'getTodosRequrest',
- }
+const getTodosRequrest = () => {
+  return {
+    type: "getTodosRequrest",
+  };
+};
+const getTodosFailed = (error) => {
+  return {
+    type: "getTodosRequrest",
+    payload: error,
+  };
+};
+const getTodosSuccess = (success) => {
+  return {
+    type: "getTodosSuccess",
+    payload: success,
+  };
+};
+
+//reducers
+const todosRenderer = (state= initialProductState, action) =>{
+
 }
-const getTodosFailed = (error) =>{
-    return{
-       type: 'getTodosRequrest',
-       payload: error
-    }
-   }
-   const getTodosSuccess = (success) => {
-    return{
-       type: 'getTodosSuccess',
-       payload: success
-    }
-   }
-      
-
-
-//renders
-
-
-
-
 
 //store
