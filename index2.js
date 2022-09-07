397733b1cb127600748d6b9051728a8219cf0bfa
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-07 21:24:13 +0600

    first commit

--- index2.js
+++ index2.js
@@ -1,51 +1,51 @@
-//middleware        /// redux-logger
+// //middleware        /// redux-logger
 
-//redux logger install korte hobe first            npm i --save redux-logger
-//this redux logger showing the output the process like how redux work
-const { createStore , applyMiddleware} = require("redux");
-const { default: logger } = require("redux-logger");
+// //redux logger install korte hobe first            npm i --save redux-logger
+// //this redux logger showing the output the process like how redux work
+// const { createStore , applyMiddleware} = require("redux");
+// const { default: logger } = require("redux-logger");
 
-// product states
-const initialProductState = {
-  products: ["sugar", "salt"],
-  numberOfProducts: 2,
-};
+// // product states
+// const initialProductState = {
+//   products: ["sugar", "salt"],
+//   numberOfProducts: 2,
+// };
 
-// product actions
-const getProductAction = () => {
-  return {
-    type: "GET_PRODUCTS",
-  };
-};
-const addProductAction = (product) => {
-  return {
-    type: "ADD_PRODUCTS",
-    payload: product,
-  };
-};
+// // product actions
+// const getProductAction = () => {
+//   return {
+//     type: "GET_PRODUCTS",
+//   };
+// };
+// const addProductAction = (product) => {
+//   return {
+//     type: "ADD_PRODUCTS",
+//     payload: product,
+//   };
+// };
 
-const productsReducer = (state = initialProductState, action) => {
-  switch (action.type) {
-    case "GET_PRODUCTS":
-      return {
-        ...state,
-      };
-    case "ADD_PRODUCTS":
-      return {
-        products: [...state.products, action.payload],
-        numberOfProducts: state.numberOfProducts + 1,
-      };
+// const productsReducer = (state = initialProductState, action) => {
+//   switch (action.type) {
+//     case "GET_PRODUCTS":
+//       return {
+//         ...state,
+//       };
+//     case "ADD_PRODUCTS":
+//       return {
+//         products: [...state.products, action.payload],
+//         numberOfProducts: state.numberOfProducts + 1,
+//       };
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
 
-const store = createStore(productsReducer, applyMiddleware(logger));
+// const store = createStore(productsReducer, applyMiddleware(logger));
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
 
-store.dispatch(getProductAction());
-store.dispatch(addProductAction("pen"));
+// store.dispatch(getProductAction());
+// store.dispatch(addProductAction("pen"));
